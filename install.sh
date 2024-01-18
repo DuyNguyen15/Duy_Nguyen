42b8c208d660997d7bdcf6e300a9c8b3c59c0d7c
DuyNguyen15 <62365457+DuyNguyen15@users.noreply.github.com>
2024-01-18 10:50:57 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -35,12 +35,13 @@ timeouts 1 5 30 60 180 1800 15 60
 setgid 65535
 setuid 65535
 flush
-auth none
+allow * * * 80-88,8080-8088 HTTP
+allow * * * 443,8443 HTTPS
 
 users $(awk -F "/" 'BEGIN{ORS="";} {print $1 ":CL:" $2 " "}' ${WORKDATA})
 
-$(awk -F "/" '{print "auth strong\n" \
-"allow " $1 "\n" \
+$(awk -F "/" '{print "auth none\n" \
+# "allow " $1 "\n" \
 "proxy -6 -n -a -p" $4 " -i" $3 " -e"$5"\n" \
 "flush\n"}' ${WORKDATA})
 EOF
@@ -64,7 +65,7 @@ upload_proxy() {
 }
 gen_data() {
     seq $FIRST_PORT $LAST_PORT | while read port; do
-        echo "usr$(random)/pass$(random)/$IP4/$port/$(gen64 $IP6)"
+        echo "$IP4/$port/$(gen64 $IP6)"
     done
 }
 
